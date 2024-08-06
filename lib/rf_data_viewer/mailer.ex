2d5fec9fd099ae0fe305b4bdc3ca8523e20ddba9
John Young <john.young>
2024-08-05 22:33:16 -0400

    getting rid of mailer stuff

--- lib/rf_data_viewer/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule RFDataViewer.Mailer do
-  use Swoosh.Mailer, otp_app: :rf_data_viewer
-end
--- lib/rf_data_viewer_web/router.ex
+++ lib/rf_data_viewer_web/router.ex
@@ -38,7 +38,7 @@ defmodule RFDataViewerWeb.Router do
       pipe_through :browser
 
       live_dashboard "/dashboard", metrics: RFDataViewerWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      #forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
 end
