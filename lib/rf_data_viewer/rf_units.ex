db799c04be3230970230c0263883d3849cbbe1bf
John Young <john.young>
2024-08-14 16:07:37 -0400

    Implemented partial functionality for RF unit manage page; lists units with counts and links to unit page to-be-developed. Moved todo list to home heex. Still need to implement creating, deleting, and updating units but because I plan to have file upload capability I'm going to wait until I get to that point.

--- lib/rf_data_viewer/rf_units.ex
+++ lib/rf_data_viewer/rf_units.ex
@@ -4,6 +4,7 @@ defmodule RFDataViewer.RFUnits do
   """
 
   import Ecto.Query, warn: false
+  alias RFDataViewer.RFUnits.RFUnitSerialNumber
   alias RFDataViewer.Repo
 
   alias RFDataViewer.RFUnits.RFUnit
@@ -19,6 +20,28 @@ defmodule RFDataViewer.RFUnits do
   """
   def list_rf_units do
     Repo.all(RFUnit)
+  end
+
+  @doc """
+  Returns a list of all RF units in a tuple of the form {rf_unit, sn_count, data_count}
+
+  ## Examples
+
+      iex> list_rf_units_with_counts()
+      [{%RFUnit{}, 0, 0}, ...]
+  """
+  def list_rf_units_with_counts do
+    query =
+      from u in RFUnit,
+        left_join: sn in assoc(u, :serial_numbers),
+        left_join: ts in assoc(sn, :test_sets),
+        left_join: ds in assoc(ts, :data_sets),
+        left_join: g in assoc(ds, :gain),
+        left_join: v in assoc(ds, :vswr),
+        group_by: u.id,
+        select: {u, count(sn.id), count(g.id) + count(v.id)}
+
+    Repo.all(query)
   end
 
   @doc """
@@ -115,6 +138,28 @@ defmodule RFDataViewer.RFUnits do
   """
   def list_rf_unit_serial_numbers do
     Repo.all(RFUnitSerialNumber)
+  end
+
+  @doc """
+  Returns a get of all serial numbers associated with a given RF Unit in a tuple of the form {sn, test_set_count, data_count}
+
+  ## Examples
+
+      iex> get_rf_unit_serial_numbers_with_counts(1)
+      [{%RFUnitSerialNumber{}, 0, 0}, ...]
+  """
+  def get_rf_unit_serial_numbers_with_counts(rf_unit_id) do
+    query =
+      from sn in RFUnitSerialNumber,
+        left_join: ts in assoc(sn, :test_sets),
+        left_join: ds in assoc(ts, :data_sets),
+        left_join: g in assoc(ds, :gain),
+        left_join: v in assoc(ds, :vswr),
+        where: sn.rf_unit_id == ^rf_unit_id,
+        group_by: sn.id,
+        select: {sn, count(ts.id), count(g.id) + count(v.id)}
+
+    Repo.all(query)
   end
 
   @doc """
--- lib/rf_data_viewer_web/live/rf_units_manage_live.ex
+++ lib/rf_data_viewer_web/live/rf_units_manage_live.ex
@@ -1,8 +1,9 @@
 defmodule RFDataViewerWeb.RFUnitsManageLive do
+  alias RFDataViewer.RFUnits
   use RFDataViewerWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    rf_units = RFUnits.list_rf_units_with_counts()
+    {:ok, assign(socket, :rf_units, rf_units)}
   end
-
 end
